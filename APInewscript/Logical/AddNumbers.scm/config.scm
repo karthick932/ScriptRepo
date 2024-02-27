6c7d8151a38ed41511b86acb7d873e419c16cdb1
Karthick Balu <karthick_ba@prod.hclpnp.com>
2024-02-27 12:00:46 +0530

    Added a new API project

---
+++ APInewscript/Logical/AddNumbers.scm/config.scm
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<editableResource createdTimestamp="1709011387833"
+    createdUser="karthick_ba" id="-4683d6b8:18de9039dd6:-7eb8"
+    type="service_component_resource" updatedTimestamp="1709011387833"
+    updatedUser="karthick_ba" version="1.1">
+    <loggingConfiguration debug="false" resultWriterLoggingLevel="FULL" writeStubEventsToDB="false"/>
+</editableResource>
